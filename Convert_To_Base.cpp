076ad5b4e95768020489ace638c1067683ee367e
NikGolovan <mykolagolovan@gmail.com>
2017-09-16 12:57:06 +0200

    Create Convert_To_Base.cpp

---
+++ Convert_To_Base.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+void convertToBase(int number, int base)
+{
+	const int MAX = 100;
+  	int BASE = 0;
+	int array[MAX];
+	int rest[MAX];
+	int i = 0, j;
+
+	/* Calculating the result and showing the dividers */
+	cout << "Dividers: ";
+	while (number > 0)
+	{
+		array[i] = number % BASE;
+		rest[i] = array[i]; 
+		i++;
+		number /= BASE;
+		cout << number << " ";
+	}
+	
+	/* Calculating the rest of previous devision */
+	cout << "\nThe rest of the division: ";
+	for (int j = 0; j < i; j++)
+	{
+		cout << rest[j] << " ";
+	}
+
+	cout << "\nThe result: ";
+	
+	/* Print the result */		
+	for (c = c-1; c >= 0; c--)
+	{
+		cout << array[c];
+	}
+	
+ 	 /* Skipping the line to print clearly the total result */
+	cout << "\n" << endl; 
+}
