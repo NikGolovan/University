85793d87d4cc584c35e4d1fd62c8420d427b42b5
NikGolovan <mykolagolovan@gmail.com>
2017-09-28 13:11:14 +0200

    Update Multiplacation_table.cpp

--- C/Multiplacation_table.cpp
+++ C/Multiplacation_table.cpp
@@ -15,7 +15,7 @@ int main() {
 
 	for (i=1; i <= MAX; i++) {  //left number
 		if (j >= MAX) //when right number is equal or higher than 10
-		cout << "\nTable de " << i << endl; //creates a phrase with new number of multiplication table
+		cout << "\nTable of " << i << endl; //creates a phrase with new number of multiplication table
 			for (j=1; j <= MAX; j++) //right number
 			cout << i << " x "<< j << " = " << i*j << endl; //shows the actual table with its result
 	}	
